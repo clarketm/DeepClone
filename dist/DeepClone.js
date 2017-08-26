f7117e815a976c70ed535c387b401adc2146597b
Travis Clarke <travis.m.clarke@gmail.com>
2017-08-25 18:58:24 -0700

    add npm "build" script

--- dist/DeepClone.js
+++ dist/DeepClone.js
@@ -4,73 +4,7 @@
 	(global.DeepClone = factory());
 }(this, (function () { 'use strict';
 
-/*!
- *
- * DeepClone.js
- *
- * Author: Travis Clarke (travis.m.clarke@travismclarke.com)
- * Date: Fri, Aug 25, 2017
- * Version: 1.0.0
- *
- * The MIT License (MIT)
-
- * Copyright (c) 2017 - Travis Clarke - https://www.travismclarke.com
-
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
-
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
-
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- */
-
-function DeepClone(obj) {   
-  let copy;    
-
-  // Handle null or undefined
-  if (obj === null || typeof obj !== "object") {
-    return obj;    
-  }
-
-  // Handle Date   
-  if (obj instanceof Date) {       
-    copy = new Date(obj.valueOf());
-    return copy;   
-  }    
-
-  // Handle Array   
-  if (obj instanceof Array) {       
-    copy = obj.slice(0);
-    return copy;   
-  }    
-
-  // Handle Object   
-  if (obj instanceof Object) {       
-    copy = {};       
-    for (let attr in obj) {           
-      if (obj.hasOwnProperty(attr)) {
-        // Recurse!
-        copy[attr] = DeepClone(obj[attr]);       
-      }
-    }       
-    return copy;   
-  }    
-
-  // Handle Error
-  throw new Error(`Unable to copy object: ${obj}`);
-}
+function DeepClone(a){let b;if(null===a||"object"!=typeof a)return a;if(a instanceof Date)return b=new Date(a.valueOf()),b;if(a instanceof Array)return b=a.slice(0),b;if(a instanceof Object){for(let c in b={},a)a.hasOwnProperty(c)&&(b[c]=DeepClone(a[c]));return b}throw new Error(`Unable to copy object: ${a}`)}
 
 return DeepClone;
 
